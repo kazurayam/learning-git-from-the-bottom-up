105651e552daff7763789b9ca95fbca71611b8c3
kazurayam <kazuaki.urayama@gmail.com>
2021-05-21 17:49:09 +0900

    imporoved draw_graph_branches.py

--- draw_graph_branches.py
+++ draw_graph_branches.py
@@ -1,38 +1,40 @@
 from graphviz import Digraph
 
-# このグラフをgraphvizで描いてみよう
+# branchesのグラフをgraphvizで描いてみよう
 g = Digraph("main", comment="Branching and the power of rebase")
 g.attr(rankdir="RL")
-g.node("0", "", shape="circle")
 
-m = Digraph("master")
-m.node("A", "A", shape="circle")
-m.node("B", "B", shape="circle")
-m.node("C", "C", shape="circle")
-m.node("D", "D", shape="circle")
-m.edge("A", "0", constraint="true")
-m.edge("B", "A", constraint="true")
-m.edge("C", "B", constraint="true")
-m.edge("D", "C", constraint="true")
-
-d = Digraph("develop")
-d.node("W", "W", shape="circle")
-d.node("X", "X", shape="circle")
-d.node("Y", "Y", shape="circle")
-d.node("Z", "Z", shape="circle")
-d.edge("W", "A", constraint="true")
-d.edge("X", "W", constraint="true")
-d.edge("Y", "X", constraint="true")
-d.edge("Z", "Y", constraint="true")
-
-b = Digraph("branches")
+c = Digraph("cluster_0")
+c.attr(label="Commits", color="lightgrey")
+c.node("0", "", shape="circle")
+c.node("A", "A", shape="circle")
+c.node("B", "B", shape="circle")
+c.node("C", "C", shape="circle")
+c.node("D", "D", shape="circle")
+#
+c.edge("A", "0", constraint="true")
+c.edge("B", "A", constraint="true")
+c.edge("C", "B", constraint="true")
+c.edge("D", "C", constraint="true")
+#
+c.node("W", "W", shape="circle")
+c.node("X", "X", shape="circle")
+c.node("Y", "Y", shape="circle")
+c.node("Z", "Z", shape="circle")
+#
+c.edge("W", "A", constraint="true")
+c.edge("X", "W", constraint="true")
+c.edge("Y", "X", constraint="true")
+c.edge("Z", "Y", constraint="true")
+#
+b = Digraph("cluster_2")
+b.attr(color="white")
 b.node("MASTER", "branch: master", shape="larrow")
 b.edge("MASTER", "D", label="HEAD", dir="none", constraint="true", style="dotted")
 b.node("DEVELOP", "branch: develop", shape="larrow")
 b.edge("DEVELOP", "Z", label="HEAD", dir="none", constraint="true", style="dotted")
 
-g.subgraph(m)
-g.subgraph(d)
+g.subgraph(c)
 g.subgraph(b)
 
 print(g.source)
